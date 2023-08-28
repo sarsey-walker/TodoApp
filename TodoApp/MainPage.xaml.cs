dd7714fb76fadacd70fd08a3169a022d6a489326
walker <sarsey.james@gmail.com>
2023-08-28 19:32:05 -0300

    Retirado loading chato quando app inicia

--- TodoApp/MainPage.xaml.cs
+++ TodoApp/MainPage.xaml.cs
@@ -1,9 +1,31 @@
-﻿namespace TodoApp;
+﻿using Microsoft.AspNetCore.Components.WebView.Maui;
+
+namespace TodoApp;
 
 public partial class MainPage : ContentPage
 {
 	public MainPage()
 	{
 		InitializeComponent();
-	}
+
+        BlazorWebViewHandler.BlazorWebViewMapper.AppendToMapping("CustomBlazorWebViewMapper", (handler, view) =>
+        {
+#if WINDOWS
+            //Workaround for WinUI splash screen
+            if (AppInfo.Current.RequestedTheme == AppTheme.Dark)
+            {
+                handler.PlatformView.DefaultBackgroundColor = Microsoft.UI.Colors.Black;
+            }
+#endif
+
+#if IOS || MACCATALYST
+            handler.PlatformView.BackgroundColor = UIKit.UIColor.Clear;
+            handler.PlatformView.Opaque = false;
+#endif
+
+#if ANDROID
+            handler.PlatformView.SetBackgroundColor(Android.Graphics.Color.Transparent);
+#endif
+        });
+    }
 }
