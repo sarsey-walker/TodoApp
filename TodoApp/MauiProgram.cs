3217f87f25af0fb7b94394468bafc3a1737a6383
walker <sarsey.james@gmail.com>
2023-09-08 18:51:58 -0300

    Instalado comunity toolkit

--- TodoApp/MauiProgram.cs
+++ TodoApp/MauiProgram.cs
@@ -1,3 +1,4 @@
+﻿using CommunityToolkit.Maui;
 ﻿using Microsoft.Extensions.Logging;
 using MudBlazor.Services;
 using TodoApp.Repository;
@@ -11,9 +12,13 @@ public static class MauiProgram
 		var builder = MauiApp.CreateBuilder();
 		builder
 			.UseMauiApp<App>()
+            // Initialize the .NET MAUI Community Toolkit by adding the below line of code
+            .UseMauiCommunityToolkit()
+            // After initializing the .NET MAUI Community Toolkit, optionally add additional fonts
 			.ConfigureFonts(fonts =>
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
 			});
 
 		builder.Services.AddMauiBlazorWebView();
