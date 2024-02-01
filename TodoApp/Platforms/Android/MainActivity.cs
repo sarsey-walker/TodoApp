e83a79627ef5cad28033074c9cb9259d5cb51541
Walker <sarsey.james@gmailk.com>
2024-02-01 08:27:39 -0300

    feat: Experiment overwriting back button

--- TodoApp/Platforms/Android/MainActivity.cs
+++ TodoApp/Platforms/Android/MainActivity.cs
@@ -2,6 +2,8 @@
 using Android.Content.PM;
 using Android.OS;
 using Android.Views;
+using Android.Widget;
+using AndroidX.Activity;
 
 namespace TodoApp;
 
@@ -14,8 +16,43 @@ public class MainActivity : MauiAppCompatActivity
         if (e.KeyCode == Keycode.Back)
         {
             // override button dispatch and disable back button functionality
-            return true;
+            //return true;
+            var navigation = Microsoft.Maui.Controls.Application.Current?.MainPage?.Navigation;
         }
         return base.DispatchKeyEvent(e);
+    }
+    protected override void OnCreate(Bundle? savedInstanceState)
+    {
+        base.OnCreate(savedInstanceState);
+        OnBackPressedDispatcher.AddCallback(this, new BackPress(this));
+    }
+}
+class BackPress : OnBackPressedCallback
+{
+    private readonly Activity activity;
+    private long backPressed;
+
+    public BackPress(Activity activity) : base(true)
+    {
+        this.activity = activity;
+    }
+
+    public override void HandleOnBackPressed()
+    {
+        var navigation = Microsoft.Maui.Controls.Application.Current?.MainPage?.Navigation;
+        if (navigation is not null && navigation.NavigationStack.Count <= 1 && navigation.ModalStack.Count <= 0)
+        {
+            const int delay = 2000;
+            if (backPressed + delay > DateTimeOffset.UtcNow.ToUnixTimeMilliseconds())
+            {
+                activity.FinishAndRemoveTask();
+                Process.KillProcess(Process.MyPid());
+            }
+            else
+            {
+                Toast.MakeText(activity, "Close", ToastLength.Long)?.Show();
+                backPressed = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+            }
+        }
     }
 }
